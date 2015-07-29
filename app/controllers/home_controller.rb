f54460c1a2888a44d52d50e8b101915d908dda6c
Stan <kulyavtsev@yahoo.com>
2015-07-29 11:54:04 -0700

    Change home#search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,10 @@
 class HomeController < ApplicationController
+  respond_to :json
+
   def index
   end
 
   def search
-    uri = URI.parse("http://api.themoviedb.org/3/search/multi")
-    uri.query = {
-      api_key: '7b5e30851a9285340e78c201c4e4ab99',
-      query: params[:keyword]
-    }.to_query
-
-    result = MultiJson.load(Net::HTTP.get_response(uri).body)
+    respond_with Search.new(params[:text]).result, location: search_path
   end
 end
