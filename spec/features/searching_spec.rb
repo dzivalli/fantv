57b20f0624a51fe26286d7876a63c7aa2355e5ee
Stan <kulyavtsev@yahoo.com>
2015-07-28 22:04:18 -0700

    Change home#index

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Searching' do
   scenario 'User input nonexistent name', js: true do
     visit root_path
-    fill_in 'search', with: 'qqqqqqqqqqqqqqqqqq'
+    fill_in 'text', with: 'qqqqqqqqqqqqqqqqqq'
 
     within '.results' do
       expect(page).to have_content 'Nothing found'
