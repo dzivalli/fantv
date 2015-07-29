1672668db8145f7f4441580078c085be0da560a2
Stan <kulyavtsev@yahoo.com>
2015-07-29 15:08:50 -0700

    Add scenario to Search spec

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -3,10 +3,35 @@ require 'rails_helper'
 feature 'Searching' do
   scenario 'User input nonexistent name', js: true do
     visit root_path
-    fill_in 'text', with: 'qqqqqqqqqqqqqqqqqq'
+    fill_in 'text', with: 'qqqqqqq'
 
-    within '.results' do
+    expect(page).to have_selector '.results', visible: false
+
+    within '.message' do
       expect(page).to have_content 'Nothing found'
+    end
+  end
+
+  scenario 'User is typing', js: true do
+    visit root_path
+    fill_in 'text', with: 'clint'
+
+    expect(page).to have_selector '.results', visible: true
+
+    within '.message' do
+      expect(page).to_not have_content 'Nothing found'
+    end
+
+    within '.movies' do
+      expect(page).to have_selector '.entity', count: 7
+    end
+
+    within '.shows' do
+      expect(page).to have_selector '.entity', count: 10
+    end
+
+    within '.people' do
+      expect(page).to have_selector '.entity', count: 2
     end
   end
 end
