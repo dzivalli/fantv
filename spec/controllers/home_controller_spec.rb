4fcef9a1e6ea610952bad211660aae3e7d985eee
Stan <kulyavtsev@yahoo.com>
2015-07-28 23:18:35 -0700

    Add HomeController#search spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe HomeController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'POST #search' do
+    context 'when nothing found' do
+      before { post :search, text: 'qqqqqqqqqqqq', format: :json }
+
+      it 'returns status 200' do
+        expect(response).to be_success
+      end
+
+      it 'includes total results equal 0' do
+        expect(response.body).to be_json_eql(%({"total_results":0}))
+      end
+    end
+
+    context 'when something found 'do
+    end
+  end
 end
