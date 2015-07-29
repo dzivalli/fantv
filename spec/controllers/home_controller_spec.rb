f1ea172110c7da9274d14d862eb69d97d8420949
Stan <kulyavtsev@yahoo.com>
2015-07-29 11:53:10 -0700

    Add context to HomeController#search spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -18,11 +18,32 @@ RSpec.describe HomeController, type: :controller do
       end
 
       it 'includes total results equal 0' do
-        expect(response.body).to be_json_eql(%({"total_results":0}))
+        expect(response.body).to be_json_eql(%({"total_results":0})).excluding('movies', 'shows', 'people')
       end
     end
 
     context 'when something found 'do
+      before { post :search, text: 'clint', format: :json }
+
+      it 'returns status 200' do
+        expect(response).to be_success
+      end
+
+      it 'includes total results equal 189' do
+        expect(response.body).to be_json_eql(%({"total_results":189})).excluding('movies', 'shows', 'people')
+      end
+
+      it 'includes movies' do
+        expect(response.body).to have_json_size(7).at_path('movies')
+      end
+
+      it 'includes shows' do
+        expect(response.body).to have_json_size(2).at_path('shows')
+      end
+
+      it 'includes people' do
+        expect(response.body).to have_json_size(11).at_path('people')
+      end
     end
   end
 end
