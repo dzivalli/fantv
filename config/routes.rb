e1f573ab1453620bb10812aa9b76ca403da86a0c
Stan <kulyavtsev@yahoo.com>
2015-07-29 15:09:33 -0700

    Add route get #search

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
   post 'search' => 'home#search'
+  get 'search' => 'home#search'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
